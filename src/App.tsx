b4873176eb45668425c5f834b291c05ac16df28d
Ankur Kedia <ankur.kedia@paytm.com>
2021-09-02 23:03:59 +0530

    fix: eslint-prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const a = 'a'
-  const b = 'a'
+  // const a = 'a';
+  // const b = 'a';
   return (
     <div className='App'>
       <header className='App-header'>
@@ -22,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
